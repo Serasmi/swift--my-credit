0890658e0aa479886ad61c5837a0df202332fda4
Sergey Smirnov <serasmi@yandex.ru>
2021-05-23 16:49:29 +0300

    Initial Commit

---
+++ swift--my-credit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift--my-credit
+//
+//  Created by Сергей Смирнов on 23.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
